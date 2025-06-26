61fd73ce9de1d6e7f2670fb8964b5e0dcf044fbd
vaibhav <vaibhav.saxena.iitd@gmail.com>
2025-06-27 02:15:14 +0530

    feat: implemented different images for web and mobile

--- Resume-project/src/components/space/space.jsx
+++ Resume-project/src/components/space/space.jsx
@@ -3,8 +3,21 @@ import React from "react";
 const Space = () => {
   return (
     <section id="space" className="min-h-screen pt-16 px-4 sm:px-6 lg:px-8">
-      <img src="/images/laptopwallgrid.png"></img>
-    </section>
+    {/* Mobile image (shown on small screens) */}
+    <img
+      src="/images/mobilewallgrid.png"
+      alt="Mobile View"
+      className="block md:hidden w-full"
+    />
+  
+    {/* Laptop/Desktop image (shown on medium screens and up) */}
+    <img
+      src="/images/desktopwallgrid.png"
+      alt="Desktop View"
+      className="hidden md:block w-full"
+    />
+  </section>
+  
   );
 };
 
